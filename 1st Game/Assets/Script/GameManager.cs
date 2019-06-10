26f4a43a100ac55171cb49cda259e7a91aabb338
Shreenivasjh <42480102+Shreenivasjh@users.noreply.github.com>
2019-06-10 21:27:06 +0530

    Add more levels

---
+++ 1st Game/Assets/Script/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+   
+    bool gameHasEnded = false;
+
+    public float RestartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    // Start is called before the first frame update
+    public void EndGame ()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", RestartDelay);
+        }
+
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ 1st Game/Assets/Script/Score.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public Text scoreText;
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
